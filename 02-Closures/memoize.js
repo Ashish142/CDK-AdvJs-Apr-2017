5a543632b69a1043680589ef34aba17baf8f3929
Magesh <tkmagesh77@gmail.com>
2017-04-05 16:50:57 +0530

    Closures

---
+++ 02-Closures/memoize.js
@@ -0,0 +1,68 @@
+/*
+create a function that checkss if the given number is a prime number or not
+Make sure the calculation logic is not executed more that once for the given number
+*/
+
+
+var isPrime = (function(){
+	var cache = {};
+	return function(n){
+		if (typeof cache[n] !== 'undefined')
+			return cache[n];
+		console.log('processing ', n);
+		cache[n] = true;
+		if (n <= 3) return cache[n];
+		for(var index = 2; index < (n/2); index++)
+			if (n % index === 0){
+				cache[n] = false;
+				break;
+			}
+		return cache[n];
+	}
+})();
+
+var isPrime = (function(){
+	var cache = {};
+
+	function checkPrime(n){
+		console.log('processing ', n);
+		if (n <= 3) return true;
+		for(var index = 2; index < (n/2); index++)
+			if (n % index === 0) return false;
+		return true;
+	}
+
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = checkPrime(n);
+		return cache[n];
+	}
+})();
+
+var isOddOrEven = (function(){
+	var cache = {};
+
+	function checkOddOrEven(n){
+		console.log('processing ', n);
+		return n % 2 === 0 ? 'even' : 'odd';
+	}
+
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = checkOddOrEven(n);
+		return cache[n];
+	}
+})();
+
+function memoize(algoFn){
+	var cache = {};
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = algoFn(n);
+		return cache[n];
+	}
+}
+
+
+
+
---
+++ 02-Closures/spinner.js
@@ -0,0 +1,26 @@
+/*
+Create an object and assign it to a variable 'spinner'
+
+the object should exhibit the following behavior
+*/
+var spinner = .....
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+var counter = 0;
+
+function up(){
+	return ++counter;
+}
+
+function down
+
